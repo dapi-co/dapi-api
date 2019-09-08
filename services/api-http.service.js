9e76d28e39f844a4aeecccd9df98f63dbf29f039
Amr Essam <amroessams@gmail.com>
2019-09-08 04:09:53 +0400

    Added correct routes

--- services/api-http.service.js
+++ services/api-http.service.js
@@ -12,7 +12,34 @@ module.exports = {
     },
     routes: [
       {
-        path: '*',
+        path: '',
+        onBeforeCall(ctx, route, req, res) {
+          if (req.method === 'GET') {
+            res.writeHead(302, {
+              Location: 'https://' + req.headers.host + req.originalUrl,
+            })
+            return res.end()
+          } else {
+            res.writeHead(505)
+            return res.end(
+              JSON.stringify(
+                {
+                  msg: 'Please use HTTPS when talking to dapi.co',
+                  success: false,
+                },
+                null,
+                2,
+              ),
+            )
+          }
+        },
+        bodyParsers: {
+          json: { strict: false },
+          urlencoded: { extended: false },
+        },
+      },
+      {
+        path: '/v1',
         onBeforeCall(ctx, route, req, res) {
           if (req.method === 'GET') {
             res.writeHead(302, {
