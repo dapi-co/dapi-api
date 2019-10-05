ff2bed5aa0f9e0ab55ba179ad517341752a8a73d
bloeys <bloeys@outlook.com>
2019-10-06 00:44:39 +0400

    Prometheus metrics + .env.dev

--- moleculer.dev.config.js
+++ moleculer.dev.config.js
@@ -1,31 +1,15 @@
 const os = require('os')
 module.exports = {
   nodeID: 'api-' + os.hostname().toLowerCase() + '-' + process.pid,
-  logger: true,
-  logLevel: 'info',
-
+  logger: {
+    type: "Pino",
+    options: {
+      level: "info"
+    }
+  },
   transporter: {
-    type: 'NATS',
-    options: {
-      urls: ['nats://10.0.0.4:4222', 'nats://10.0.0.5:4222'],
-      user: 'nats',
-      pass: 'LXl&3rb@4s#e1JAb1uA6fTFX#zJK@eF$',
-    },
+    type: 'NATS'
   },
-
-  cacher: {
-    type: 'Redis',
-    options: {
-      prefix: 'MOL-API',
-      monitor: true,
-      redis: {
-        host: 'dapi.redis.cache.windows.net',
-        port: 6380,
-        password: 'NZr7JZ8nmK1FaYfXQnmgnv+nVyxsRSZaStRDtQ1pHRY=',
-      },
-    },
-  },
-
   metrics: {
     enabled: true,
     reporter: [
@@ -33,7 +17,7 @@ module.exports = {
         type: "Prometheus",
         options: {
           port: 3030,
-          path: "http://40.123.212.110/metrics",
+          path: "/metrics",
           defaultLabels: registry => ({
             namespace: registry.broker.namespace,
             nodeID: registry.broker.nodeID
