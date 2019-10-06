4e2ce041e20ed5da5dba48ef4b466b40df5e56cb
bloeys <bloeys@outlook.com>
2019-10-06 06:41:47 +0400

    Force service update

--- ci/scripts/update-scale-set.sh
+++ ci/scripts/update-scale-set.sh
@@ -9,5 +9,5 @@ az login --service-principal \
 
 az vmss list-instances -n "$AZURE_SS_NAME" -g $AZURE_RG --query "[].id" --output tsv | \
 az vmss run-command invoke --command-id "RunShellScript" \
---scripts "sudo docker service update -d --image $CR_DOMAIN/$SERVICE $SERVICE" \
+--scripts "sudo docker service update -d --force --image $CR_DOMAIN/$SERVICE $SERVICE" \
 --ids @-
