63af00874f3e994e06ac88bafd226da2e8c8fb9b
bloeys <bloeys@outlook.com>
2019-10-07 01:02:40 +0400

    Ensure unused images are removed

--- ci/scripts/update-scale-set.sh
+++ ci/scripts/update-scale-set.sh
@@ -9,5 +9,5 @@ az login --service-principal \
 
 az vmss list-instances -n "$AZURE_SS_NAME" -g $AZURE_RG --query "[].id" --output tsv | \
 az vmss run-command invoke --command-id "RunShellScript" \
---scripts "sudo docker service update -d --force --update-order start-first --update-failure-action rollback --image $CR_DOMAIN/$SERVICE $SERVICE" \
---ids @-
+--scripts "sudo docker service update -d --force --update-order start-first --update-failure-action rollback --image $CR_DOMAIN/$SERVICE $SERVICE \
+&& sudo docker image prune -af" --ids @-
