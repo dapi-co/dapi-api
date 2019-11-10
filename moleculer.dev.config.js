4150d46d8fc40b8d5196eb811ad4f98ec6d38d46
Amr Essam <amroessams@gmail.com>
2019-11-10 15:21:28 +0400

    added dev config

--- moleculer.dev.config.js
+++ moleculer.dev.config.js
@@ -1,31 +1,7 @@
 const os = require('os')
 module.exports = {
   nodeID: 'api-' + os.hostname().toLowerCase() + '-' + process.pid,
-  logger: {
-    type: "Pino",
-    options: {
-      level: "info"
-    }
-  },
   transporter: {
-    type: 'NATS'
+    type: 'NATS',
   },
-  metrics: {
-    enabled: true,
-    reporter: [
-      {
-        type: "Prometheus",
-        options: {
-          port: 3031,
-          path: "/metrics",
-          defaultLabels: registry => ({
-            namespace: registry.broker.namespace,
-            nodeID: registry.broker.nodeID
-          }),
-          includes: ["moleculer.**"],
-          excludes: ["moleculer.transit.**"]
-        }
-      }
-    ]
-  }
 }
